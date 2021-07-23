123dd9392a47ab699e34492b4402b8015f96700f
1NT18IS018 <82028696+1NT18IS018@users.noreply.github.com>
2021-07-23 18:47:13 +0530

    udp server

--- udpserver.c
+++ udpserver.c
@@ -1,32 +1,43 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-main()
-{
-    struct sockaddr_in client,server;
-    int s,n;
-    char b1[100],b2[100];
-    s=socket(AF_INET,SOCK_DGRAM,0);
-    server.sin_family=AF_INET;
-    server.sin_port=2000;
-    server.sin_addr.s_addr=inet_addr("127.0.0.1");
-    bind(s,(struct sockaddr *)&server,sizeof(server));
-    printf("\nServer ready,waiting for client....\n");
-    n=sizeof(client);
-    while(1)
-    {
-        recvfrom(s,b1,sizeof(b1),0,(struct sockaddr *) &client,&n);
-        if(!(strcmp(b1,"end")))
-            break;
-        printf("\nClient:%s",b1);
-        printf("\nServer:");
-        gets(b2);
-        sendto(s,b2,sizeof(b2),0,(struct sockaddr *) &client,n);
-              
-    }
- 
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(){
+  int clientSocket, portNum, nBytes;
+  char buffer[1024];
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+
+  /*Create UDP socket*/
+  clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+
+  /*Configure settings in address struct*/
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(8893);
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  /*Initialize size variable to be used later on*/
+  addr_size = sizeof serverAddr;
+
+  while(1){
+    printf("Type a sentence to send to server:\n");
+    fgets(buffer,1024,stdin);
+    printf("You typed: %s",buffer);
+
+    nBytes = strlen(buffer) + 1;
+    
+    /*Send message to server*/
+    sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
+
+    /*Receive message from server*/
+                nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
+
+    printf("Received from server: %s\n",buffer);
+
+  }
+
+  return 0;
 }
